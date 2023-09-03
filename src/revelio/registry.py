7730bcb38b681a9141d0f72241921cadf6625d63
ndido98 <ndido98@gmail.com>
2023-09-03 10:04:21 +0000

    chore: add namespacing to the registry

--- src/revelio/registry.py
+++ src/revelio/registry.py
@@ -1,7 +1,7 @@
 """
 The registry keeps track of all the classes that are registered, and can then be instantiated via a string name.
 
-Classes must have globally unique names (case-insensitive) in order to be registered.
+Classes must have namespace-wise unique names (case-insensitive) in order to be registered.
 """
 
 from __future__ import annotations
@@ -13,58 +13,80 @@ __all__ = (
 )
 
 
-__revelio_registry__: dict[str, type] = {}
+__revelio_registry__: dict[str, dict[str, type]] = {}
 
 
-def clear() -> None:
+def clear(namespace: str | None = None) -> None:
     """
-    Clears the registry.
-    """
-    __revelio_registry__.clear()
-
-
-def register(name: str, cls: type) -> None:
-    """
-    Registers a class with the given name.
+    Clears a namespace in the registry, or the entire registry if no namespace is given.
 
     Arguments:
+        namespace: The namespace to clear. If None, the entire registry is cleared.
+
+    Raises:
+        TypeError: If the namespace is not a string, or None.
+    """
+    if namespace is None:
+        __revelio_registry__.clear()
+    elif isinstance(namespace, str):
+        del __revelio_registry__[namespace]
+    else:
+        raise TypeError(f"Namespace must be a string or None, got {type(namespace).__name__}.")
+
+
+def register(namespace: str, name: str, cls: type) -> None:
+    """
+    Registers a class with the given name under the given namespace.
+
+    Arguments:
+        namespace: The namespace to register the class under.
         name: The name to register the class with.
         cls: The class to register.
 
     Raises:
-        TypeError: If the name is not a string, or if the class is not a type.
+        TypeError: If the namespace or the name is not a string, or if the class is not a type.
         ValueError: If a class with the given name is already registered.
     """
+    if not isinstance(namespace, str):
+        raise TypeError(f"Namespace must be a string, got {type(namespace).__name__}.")
     if not isinstance(name, str):
         raise TypeError(f"Name must be a string, got {type(name).__name__}.")
     if not isinstance(cls, type):
         raise TypeError(f"Class must be a type, got {type(cls).__name__}.")
     name = _sanitize_name(name)
-    if name in __revelio_registry__ and __revelio_registry__[name] is not cls:
-        raise ValueError(f"Class with sanitized name '{name}' already registered.")
-    __revelio_registry__[name] = cls
+    namespace_registry = __revelio_registry__.get(namespace, {})
+    if name in namespace_registry and namespace_registry[name] is not cls:
+        raise ValueError(
+            f"A class with the sanitized name '{name}' is already registered in the '{namespace}' namespace."
+        )
+    namespace_registry[name] = cls
+    __revelio_registry__[namespace] = namespace_registry
 
 
-def get(name: str) -> type:
+def get(namespace: str, name: str) -> type:
     """
-    Gets the class with the given name.
+    Gets the class with the given name from the given namespace.
 
     Arguments:
+        namespace: The namespace to get the class from.
         name: The name of the class to get.
 
     Returns:
         The class with the given name.
 
     Raises:
-        TypeError: If the name is not a string.
+        TypeError: If the namespace or the name is not a string.
         KeyError: If no class with the given name is registered.
     """
+    if not isinstance(namespace, str):
+        raise TypeError(f"Namespace must be a string, got {type(namespace).__name__}.")
     if not isinstance(name, str):
         raise TypeError(f"Name must be a string, got {type(name).__name__}.")
     name = _sanitize_name(name)
-    if name not in __revelio_registry__:
-        raise KeyError(f"No class registered with sanitized name '{name}'.")
-    return __revelio_registry__[name]
+    namespace_registry = __revelio_registry__.get(namespace, {})
+    if name not in namespace_registry:
+        raise KeyError(f"Could not find a class with the sanitized name '{name}' in the '{namespace}' namespace.")
+    return namespace_registry[name]
 
 
 def _sanitize_name(name: str) -> str:
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -22,17 +22,19 @@ def cleanup_registry() -> Generator:
 
 def test_register_get() -> None:
     """
-    Test that `register` registers a class with the given name, and `get` gets it back.
+    Test that `register` registers a class with the given namespace and name, and `get` gets it back.
     """
 
     class MyClass:  # pylint: disable=missing-class-docstring
         pass
 
-    registry.register("MyClass", MyClass)
-    assert registry.get("MyClass") is MyClass
-    assert registry.get("myclass") is MyClass
-    assert registry.get("my-class") is MyClass
-    assert registry.get("my_class") is MyClass
+    registry.register("global", "MyClass", MyClass)
+    assert registry.get("global", "MyClass") is MyClass
+    assert registry.get("global", "myclass") is MyClass
+    assert registry.get("global", "my-class") is MyClass
+    assert registry.get("global", "my_class") is MyClass
+    with pytest.raises(KeyError):
+        registry.get("other", "MyClass")
 
 
 def test_register_incorrect_types() -> None:
@@ -44,15 +46,16 @@ def test_register_incorrect_types() -> None:
         pass
 
     with pytest.raises(TypeError):
-        registry.register("test", "fail")
-
+        registry.register("global", "test", "fail")
     with pytest.raises(TypeError):
-        registry.register(0, MyClass)
+        registry.register("global", 0, MyClass)
+    with pytest.raises(TypeError):
+        registry.register(0, "test", MyClass)
 
 
-def test_register_two_classes_same_name() -> None:
+def test_register_two_classes_same_namespace_same_name() -> None:
     """
-    Test that `register` raises a ValueError when registering two classes with the same name.
+    Test that `register` raises a ValueError when registering two classes with the same name under the same namespace.
     """
 
     class MyClass1:  # pylint: disable=missing-class-docstring
@@ -61,9 +64,27 @@ def test_register_two_classes_same_name() -> None:
     class MyClass2:  # pylint: disable=missing-class-docstring
         pass
 
-    registry.register("MyClass", MyClass1)
+    registry.register("global", "MyClass", MyClass1)
     with pytest.raises(ValueError):
-        registry.register("MyClass", MyClass2)
+        registry.register("global", "MyClass", MyClass2)
+
+
+def test_register_two_classes_different_namespace_same_name() -> None:
+    """
+    Test that `register` completes successfully when registering two classes with the same name
+    under different namespaces.
+    """
+
+    class MyClass1:  # pylint: disable=missing-class-docstring
+        pass
+
+    class MyClass2:  # pylint: disable=missing-class-docstring
+        pass
+
+    registry.register("global", "MyClass", MyClass1)
+    registry.register("other", "MyClass", MyClass2)
+    assert registry.get("global", "MyClass") is MyClass1
+    assert registry.get("other", "MyClass") is MyClass2
 
 
 def test_get_incorrect_types() -> None:
@@ -71,7 +92,9 @@ def test_get_incorrect_types() -> None:
     Test that `get` raises a TypeError when given incorrect types.
     """
     with pytest.raises(TypeError):
-        registry.get(0)
+        registry.get("global", 0)
+    with pytest.raises(TypeError):
+        registry.get(0, "test")
 
 
 def test_get_not_existing() -> None:
@@ -79,19 +102,31 @@ def test_get_not_existing() -> None:
     Test that `get` raises a KeyError when given a name that does not exist.
     """
     with pytest.raises(KeyError):
-        registry.get("not_existing")
+        registry.get("global", "not_existing")
 
 
 def test_clear() -> None:
     """
-    Test that `clear` clears the registry.
+    Test that `clear` clears the registry or a single namespace.
     """
 
-    class MyClass:  # pylint: disable=missing-class-docstring
+    class MyClass1:  # pylint: disable=missing-class-docstring
         pass
 
-    registry.register("MyClass", MyClass)
-    assert registry.get("MyClass") is MyClass
+    class MyClass2:  # pylint: disable=missing-class-docstring
+        pass
+
+    registry.register("global", "MyClass", MyClass1)
+    registry.register("other", "MyClass", MyClass2)
+    assert registry.get("global", "MyClass") is MyClass1
+    assert registry.get("other", "MyClass") is MyClass2
+    registry.clear("global")
+    with pytest.raises(KeyError):
+        registry.get("global", "MyClass")
+    assert registry.get("other", "MyClass") is MyClass2
+    registry.register("global", "MyClass", MyClass1)
     registry.clear()
     with pytest.raises(KeyError):
-        registry.get("MyClass")
+        registry.get("global", "MyClass")
+    with pytest.raises(KeyError):
+        registry.get("other", "MyClass")
